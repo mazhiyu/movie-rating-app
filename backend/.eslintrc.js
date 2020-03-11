304970e06d69576da8a4c8c4863c7fc434afab3d
mazhiyu <mazhiyu@foxmail.com>
2020-03-11 12:18:34 +0800

    refactor(backend): delete redundancy configuration for `semi`

--- backend/.eslintrc.js
+++ backend/.eslintrc.js
@@ -2,20 +2,15 @@ module.exports = {
   env: {
     commonjs: true,
     es6: true,
-    node: true
+    node: true,
   },
-  extends: [
-    'standard',
-    "plugin:prettier/recommended"
-  ],
+  extends: ['standard', 'plugin:prettier/recommended'],
   globals: {
     Atomics: 'readonly',
-    SharedArrayBuffer: 'readonly'
+    SharedArrayBuffer: 'readonly',
   },
   parserOptions: {
-    ecmaVersion: 2018
+    ecmaVersion: 2018,
   },
-  rules: {
-    semi: ["error", "always"]
-  }
-}
+  rules: {},
+};
--- backend/.prettierrc.js
+++ backend/.prettierrc.js
@@ -1,4 +1,5 @@
 module.exports = {
-  "singleQuote": true,
-  "trailingComma": "es5"
-}
+  singleQuote: true,
+  trailingComma: 'es5',
+  semi: true, // default is true
+};
